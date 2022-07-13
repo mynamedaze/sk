77d33fe98859851e18f4afc09845cc7d73cf098b
Murat Kudainetov <mynamedaze@gmail.com>
2022-07-13 16:47:04 +0300

    слайдер по кейсам делаю

---
+++ serviskomfort/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+$(document).ready(function () {
+    $('.cases__list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        items: 1,
+        dots: false,
+        nav: true
+    });
+});
