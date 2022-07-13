a3ad61160de0883ac3d084de96e3e256458b74a2
Murat Kudainetov <murat.kudainetov@gmail.com>
2022-07-13 23:39:32 +0300

    осталось сделать индивидуальные блоки

--- serviskomfort/js/main.js
+++ serviskomfort/js/main.js
@@ -3,8 +3,14 @@ $(document).ready(function () {
     $('.cases__list').owlCarousel({
         loop: true,
         autoplay: true,
+        autoplayTimeout: 10000,
         items: 1,
         dots: false,
-        nav: true
+        nav: true,
+        responsive: {
+            1200: {
+                mouseDrag: false
+            }
+        }
     });
 });
